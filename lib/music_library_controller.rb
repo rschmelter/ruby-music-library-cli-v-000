aed3b7fd168c9b1a19ee8001c10f76d9a531d25d
Ryan Schmelter <rschmelter33@gmail.com>
2017-12-22 00:13:27 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -68,7 +68,7 @@ class MusicLibraryController
 
     @input = gets.strip.to_i
     if (1..Song.all.length).include?(@input)
-      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+      song = Song.all.sort{ |a, b| a.name <=> b.name }[@input - 1]
     end
 
     puts "Playing #{song.name} by #{song.artist.name}" if song
